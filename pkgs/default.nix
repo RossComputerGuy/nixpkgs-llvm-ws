b179b6e885aaed134b1a01b9cfc13d8d4baee2f2
Tristan Ross <tristan.ross@midstall.com>
2024-06-15 20:45:22 -0700

    feat(pkgs): add kexec-tools

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -9,4 +9,15 @@ lib: final: prev:
   inherit (prev.callPackages ./os-specific/linux/apparmor {})
     libapparmor apparmor-utils apparmor-bin-utils apparmor-parser apparmor-pam
     apparmor-profiles apparmor-kernel-patches apparmorRulesFromClosure;
+
+  # PR: https://github.com/NixOS/nixpkgs/pull/317942
+  kexec-tools = (prev.kexec-tools.override { inherit (final) stdenv; }).overrideAttrs (f: p: {
+    patches = p.patches ++ [
+      (prev.fetchpatch {
+        name = "fix-purgatory-llvm-libunwind.patch";
+        url = "https://raw.githubusercontent.com/NixOS/nixpkgs/de926d60457c7b455f53605d3f38f6dd28333cb0/pkgs/os-specific/linux/kexec-tools/fix-purgatory-llvm-libunwind.patch";
+        hash = "sha256-iSVHmHNWDjBRKVOzCpJhZfIqs6E+ERhCVGQqxR/cAKo=";
+      })
+    ];
+  });
 }
