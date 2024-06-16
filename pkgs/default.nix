99f983b791c787e0b06774176b94cf7a5717591e
Tristan Ross <tristan.ross@midstall.com>
2024-06-16 00:32:46 -0700

    feat(pkgs): update libbsd

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,7 +2,8 @@ lib: final: prev:
 {
   # PR: https://github.com/NixOS/nixpkgs/pull/319976
   libbsd = prev.libbsd.overrideAttrs (f: p: {
-    NIX_LDFLAGS = lib.optionalString (final.stdenv.cc.bintools.isLLVM && lib.versionAtLeast final.stdenv.cc.bintools.version "17") "--undefined-version";
+    configureFlags = p.configureFlags ++
+      lib.optional (final.stdenv.cc.bintools.isLLVM && lib.versionAtLeast final.stdenv.cc.bintools.version "17") "LDFLAGS=-Wl,--undefined-version";
   });
 
   # PR: https://github.com/NixOS/nixpkgs/pull/304753
