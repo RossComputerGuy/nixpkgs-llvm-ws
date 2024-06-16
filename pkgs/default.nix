0e5ca3aaa9355a50e4fe391b1b255665040e8199
Tristan Ross <tristan.ross@midstall.com>
2024-06-15 20:11:51 -0700

    feat(pkgs): add apparmor

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,4 +4,9 @@ lib: final: prev:
   libbsd = prev.libbsd.overrideAttrs (f: p: {
     NIX_LDFLAGS = lib.optionalString (final.stdenv.cc.bintools.isLLVM && lib.versionAtLeast final.stdenv.cc.bintools.version "17") "--undefined-version";
   });
+
+  # PR: https://github.com/NixOS/nixpkgs/pull/304753
+  inherit (prev.callPackages ./os-specific/linux/apparmor {})
+    libapparmor apparmor-utils apparmor-bin-utils apparmor-parser apparmor-pam
+    apparmor-profiles apparmor-kernel-patches apparmorRulesFromClosure;
 }
