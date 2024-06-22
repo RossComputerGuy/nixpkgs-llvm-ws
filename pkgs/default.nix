c3a43fd46753a6d20eb317306bd49812f681d67f
Tristan Ross <tristan.ross@midstall.com>
2024-06-22 07:25:29 -0700

    fix(pkgs): sync rustc

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -38,6 +38,13 @@ lib: final: prev:
     rustc-unwrapped = prev.rustc.unwrapped.overrideAttrs (f: p: {
       configureFlags = p.configureFlags
         ++ [ "--llvm-libunwind=in-tree" ];
+
+      buildInputs = p.buildInputs
+        ++ [ (final.runCommandLocal "libunwind-libgcc" {} ''
+          mkdir -p $out/lib
+          ln -s ${final.llvmPackages.libunwind}/lib/libunwind.so $out/lib/libgcc_s.so
+          ln -s ${final.llvmPackages.libunwind}/lib/libunwind.so $out/lib/libgcc_s.so.1
+        '') ];
     });
   };
 
