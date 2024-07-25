94b79cb3d142df6cda6f271486dd7469032d9ad9
Tristan Ross <tristan.ross@midstall.com>
2024-07-24 21:07:43 -0700

    feat(pkgs): sync rust

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,19 +1,8 @@
-lib: final: prev:
+lib: final: prev: with final;
 {
   # PR: https://github.com/NixOS/nixpkgs/pull/320432
-  rustc = prev.rustc.override {
-    rustc-unwrapped = prev.rustc.unwrapped.overrideAttrs (f: p: {
-      configureFlags = p.configureFlags
-        ++ [ "--llvm-libunwind=system" ];
-
-      NIX_LDFLAGS = [ "--push-state --as-needed -L${final.llvmPackages.libcxx}/lib -lc++ --pop-state" ];
-
-      buildInputs = p.buildInputs
-        ++ [ (final.runCommandLocal "libunwind-libgcc" {} ''
-          mkdir -p $out/lib
-          ln -s ${final.llvmPackages.libunwind}/lib/libunwind.so $out/lib/libgcc_s.so
-          ln -s ${final.llvmPackages.libunwind}/lib/libunwind.so $out/lib/libgcc_s.so.1
-        '') ];
-    });
+  rust_1_79 = callPackage ./development/compilers/rust/1_79.nix {
+    inherit (darwin.apple_sdk.frameworks) CoreFoundation Security SystemConfiguration;
+    llvm_18 = llvmPackages_18.libllvm;
   };
 }
