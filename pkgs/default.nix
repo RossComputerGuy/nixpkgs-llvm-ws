66a4f43def967b48a7ea959d2da6d686e51d6078
Tristan Ross <tristan.ross@midstall.com>
2024-07-24 21:39:03 -0700

    feat(pkgs): add xorg.libX11

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,4 +5,12 @@ lib: final: prev: with final;
     inherit (darwin.apple_sdk.frameworks) CoreFoundation Security SystemConfiguration;
     llvm_18 = llvmPackages_18.libllvm;
   };
+
+  xorg = prev.xorg.overrideScope (f: p: {
+    # PR: https://github.com/NixOS/nixpkgs/pull/329584
+    libX11 = p.libX11.overrideAttrs (attrs: {
+       configureFlags = attrs.configureFlags or []
+          ++ lib.optional (stdenv.targetPlatform.useLLVM or false) "ac_cv_path_RAWCPP=cpp";
+    });
+  });
 }
