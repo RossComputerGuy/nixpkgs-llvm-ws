7ffa528900ad87d3ddb74b4b4770c3322dd54de9
Tristan Ross <tristan.ross@midstall.com>
2025-02-15 18:42:08 -0800

    fix: hydra jobs

--- pkgs/top-level/release.nix
+++ pkgs/top-level/release.nix
@@ -2,18 +2,25 @@ let
   nixpkgs = import ./nixpkgs.nix;
   lib = import "${nixpkgs}/lib";
 in
-args:
-import "${nixpkgs}/pkgs/top-level/release.nix" ({
-  system = builtins.currentSystem;
-} // (lib.removeAttrs args [ "nixpkgs-llvm-ws" ]) // {
-  nixpkgsArgs = (args.nixpkgsArgs or {}) // {
-    crossSystem = {
-      useLLVM = true;
-      linker = "lld";
-      system = args.system or builtins.currentSystem;
+{
+  officialRelease ? false,
+  nixpkgs-llvm-ws ? { },
+}@args:
+import "${nixpkgs}/pkgs/top-level/release.nix" (
+  {
+    system = builtins.currentSystem;
+  }
+  // (lib.removeAttrs args [ "nixpkgs-llvm-ws" ])
+  // {
+    nixpkgsArgs = (args.nixpkgsArgs or { }) // {
+      crossSystem = {
+        useLLVM = true;
+        linker = "lld";
+        system = args.system or builtins.currentSystem;
+      };
+      overlays = [
+        (import ../default.nix lib)
+      ];
     };
-    overlays = [
-      (import ../default.nix lib)
-    ];
-  };
-})
+  }
+)
